411bcb3c82e0b88fc8c3016b8aa924e9dbb17cc9
sangkamath <twinkle_kamath@yahoo.com>
2023-12-18 11:13:25 +0530

    Corrected typo

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice';
 
 export default configureStore({
-  reducer: () => ({
+  reducer: {
     posts: postsReducer
-  }),
+  },
 })
