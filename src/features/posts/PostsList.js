4efcc6b4e9a4425507cb32d65084976d94bbea76
sangkamath <twinkle_kamath@yahoo.com>
2023-12-20 10:47:21 +0530

    Add display of relative post timestamps

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
 
 export const PostsList = () => {
     const posts = useSelector((state) => state.posts);
@@ -11,6 +12,7 @@ export const PostsList = () => {
             <h3>{post.title}</h3>
             <div>
                 <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
             </div>
             <p className='post-content'>{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className="button muter-button">
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
 
 export const SinglePostPage = ({ match}) => {
     const { postId } = match.params;
@@ -24,6 +25,7 @@ export const SinglePostPage = ({ match}) => {
                 <h2>{post.title}</h2>
                 <div>
                     <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date} />
                 </div>
                 <p className="post-content">{post.content}</p>
                 <Link to={`/editPost/${post.id}`} className="button">
