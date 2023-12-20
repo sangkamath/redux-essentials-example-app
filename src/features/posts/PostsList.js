2f9f2a366af0573f0e95250ced6940fca668edfc
sangkamath <twinkle_kamath@yahoo.com>
2023-12-20 11:04:22 +0530

    Sort posts list in reverse chronological order

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,7 +7,10 @@ import { TimeAgo } from './TimeAgo';
 export const PostsList = () => {
     const posts = useSelector((state) => state.posts);
 
-    const renderedPosts = posts.map((post) => (
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+
+
+    const renderedPosts = orderedPosts.map((post) => (
         <article className='post-excerpt' key={post.id}>
             <h3>{post.title}</h3>
             <div>
