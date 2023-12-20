f0b73b64ddf2e846ec40eec3eb9d8503d2f69598
sangkamath <twinkle_kamath@yahoo.com>
2023-12-20 12:36:49 +0530

    Implement reaction button logic

--- src/features/posts/ReactionButtons.js
+++ src/features/posts/ReactionButtons.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { reactionAdded} from "./postsSlice";
 const reactionEmoji = {
    thumbsUp: '👍',
    hooray: '🎉',
@@ -8,9 +10,15 @@ const reactionEmoji = {
 }
 
 export const ReactionButtons = ({post}) => {
+    const dispatch = useDispatch();
     const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
         return (
-            <button key={name} type="button" className="muted-button reaction-button">
+            <button 
+                key={name} t
+                type="button" 
+                className="muted-button reaction-button"
+                onClick={() => dispatch(reactionAdded({ postId: post.id, reaction: name}))}
+            >
                 {emoji} {post.reactions[name]}
             </button>
         )
