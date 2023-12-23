18c2efe9682a342150961eb54ef88de167a56ad2
sangkamath <twinkle_kamath@yahoo.com>
2023-12-23 12:44:12 +0530

    Add Notifications component and Refresh Notifications button

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Link } from'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchNotifications } from '../features/notifications/notificationsSlice';
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications())
+  }
+
   return (
     <nav>
       <section>
@@ -11,8 +19,12 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
+            <Link to="/notifications">Notifications</Link>
           </div>
         </div>
+        <button className='button' onClick={fetchNewNotifications}>
+          Refresh Notifications
+        </button>
       </section>
     </nav>
   )
