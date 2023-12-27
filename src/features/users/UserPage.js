75e7cae862d07fed93ba131511986a76250f4b20
sangkamath <twinkle_kamath@yahoo.com>
2023-12-27 13:37:32 +0530

    Add a memoized selectPostsByUser selector

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -4,15 +4,13 @@ import {Link} from "react-router-dom";
 
 import { selectUserById } from "./usersSlice";
 import { selectAllPosts } from "../posts/postsSlice";
+import { selectPostsByUser } from "../posts/postsSlice";
 
 export const UserPage = ({match}) => {
     const { userId } = match.params;
-    const user = useSelector((state) => selectUserById(state, userId));
+    const user = useSelector((state) => selectPostsByUser(state, userId));
 
-    const postsForUser = useSelector((state) => {
-        const allPosts = selectAllPosts(state);
-        return allPosts.filter((post) => post.user === userId);
-    })
+    const postsForUser = useSelector((state) => selectPostsByUser(state, userId))
 
     const postTitles = postsForUser.map((post) => (
         <li key={post.id}>
