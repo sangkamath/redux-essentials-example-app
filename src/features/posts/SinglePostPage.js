9705c804ac3490db1020cc5965a77bd40b577d3b
sangkamath <twinkle_kamath@yahoo.com>
2024-01-25 21:18:56 +0530

    Convert SinglePostPage to use RTK Query useGetPostQuery hook

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -4,25 +4,19 @@ import { Link } from "react-router-dom";
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
-import { selectPostById } from './postsSlice';
+import { useGetPostQuery } from "../api/apiSlice";
+import { Spinner } from "../../components/Spinner";
 
-export const SinglePostPage = ({ match}) => {
+export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
 
-    const post = useSelector(state =>
-       selectPostById(state, postId)
-    )
+    const { data: post, isFetching, isSuccess } = useGetPostQuery([postId]);
 
-    if(!post) {
-        return (
-            <section>
-                <h2>Post not found!</h2>
-            </section>
-        )
-    }
-
-    return (
-        <section>
+    let content;
+    if (isFetching) {
+        content = <Spinner text="Loading..." />
+    } else if (isSuccess) {
+        content = (
             <article className='post'>
                 <h2>{post.title}</h2>
                 <div>
@@ -35,6 +29,12 @@ export const SinglePostPage = ({ match}) => {
                     Edit Post
                 </Link>
             </article>
+        )
+    }
+
+    return (
+        <section>
+            {content}
         </section>
     )
 }
