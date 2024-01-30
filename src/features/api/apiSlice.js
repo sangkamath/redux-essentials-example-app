80b8391932d71bb8cea87f3b293a5995d774955b
sangkamath <twinkle_kamath@yahoo.com>
2024-01-30 19:40:58 +0530

    Add basic tag invalidation when adding a new post

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({ baseUrl : "/fakeApi"}),
+    tagTypes: ["Post"],
     endpoints: (builder) => ({
         getPosts: builder.query({
             query: () => "/posts",
+            providesTags: ["Post"]
 
         }),
         getPost: builder.query({
@@ -16,7 +18,8 @@ export const apiSlice = createApi({
                 url: "/posts",
                 method: "POST",
                 body: initialPost
-            })
+            }),
+            invalidatesTags: ["Post"]
         })
     })
 })
