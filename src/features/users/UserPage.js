ca330e3ba16ac4cbade9c8f2e65af9bee86e2690
sangkamath <twinkle_kamath@yahoo.com>
2024-02-07 12:39:53 +0530

    Switch UserPage to read posts from cache

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,27 +1,52 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import {Link} from "react-router-dom";
+import React, { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { createSelector } from '@reduxjs/toolkit'
 
-import { selectUserById } from "./usersSlice";
-import { selectAllPosts } from "../posts/postsSlice";
-import { selectPostsByUser } from "../posts/postsSlice";
+import { selectUserById } from '../users/usersSlice'
+import { useGetPostsQuery } from '../api/apiSlice'
 
-export const UserPage = ({match}) => {
-    const { userId } = match.params;
-    const user = useSelector((state) => selectPostsByUser(state, userId));
+export const UserPage = ({ match }) => {
+  const { userId } = match.params
 
-    const postsForUser = useSelector((state) => selectPostsByUser(state, userId))
+  const user = useSelector((state) => selectUserById(state, userId))
+  let userName = "";
+  if(user && user.name) {
+    userName = user.name;
+  }
 
-    const postTitles = postsForUser.map((post) => (
-        <li key={post.id}>
-            <Link to={`/posts/${post.id}`}>{post.title}</Link>
-        </li>
-    ))
-
-    return (
-        <section>
-            <h2>{user.name}</h2>
-            <ul>{postTitles}</ul>
-        </section>
+  const selectPostsForUser = useMemo(() => {
+    const emptyArray = []
+    // Return a unique selector instance for this page so that
+    // the filtered results are correctly memoized
+    return createSelector(
+      (res) => res.data,
+      (res, userId) => userId,
+      (data, userId) => data?.filter((post) => post.user === userId) ?? emptyArray
     )
+  }, [])
+
+  // Use the same posts query, but extract only part of its data
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (res) => ({
+      ...res,
+      // Include a field called `postsForUser` in the hook result object,
+      // which will be filtered list of posts
+      postsForUser: selectPostsForUser(res, userId),
+    }),
+  })
+
+  const postTitles = postsForUser.map((post) => (
+    <li key={post.id}>
+      <Link to={`/posts/${post.id}`}>{post.title}</Link>
+    </li>
+  ))
+
+  return (
+    <section>
+      <h2>{userName}</h2>
+
+      <ul>{postTitles}</ul>
+    </section>
+  )
 }
