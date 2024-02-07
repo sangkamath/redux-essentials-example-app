96e720532c59265f9df2c89bcb6cda25f3b8e3c5
sangkamath <twinkle_kamath@yahoo.com>
2024-02-07 16:19:49 +0530

    Update Reactionbuttons to use useAddreaction mutation

--- src/features/posts/ReactionButtons.js
+++ src/features/posts/ReactionButtons.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { reactionAdded} from "./postsSlice";
+import { useAddReactionMutation } from "../api/apiSlice";
 const reactionEmoji = {
-   thumbsUp: '👍',
-   hooray: '🎉',
-   heart: '❤️',
-   rocket: '🚀',
-   eyes: '👀',
+    thumbsUp: '👍',
+    hooray: '🎉',
+    heart: '❤️',
+    rocket: '🚀',
+    eyes: '👀',
 }
 
-export const ReactionButtons = ({post}) => {
-    const dispatch = useDispatch();
-    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+export const ReactionButtons = ({ post }) => {
+    const [addReaction] = useAddReactionMutation();
+    const reactionButtons = Object.entries(reactionEmoji).map(([reactionName, emoji]) => {
         return (
-            <button 
-                key={name} t
-                type="button" 
+            <button
+                key={reactionName}
+                type="button"
                 className="muted-button reaction-button"
-                onClick={() => dispatch(reactionAdded({ postId: post.id, reaction: name}))}
+                onClick={() => {
+                    addReaction({ postId: post.id, reaction: reactionName })
+                }}
             >
-                {emoji} {post.reactions[name]}
+                {emoji} {post.reactions[reactionName]}
             </button>
         )
     })
