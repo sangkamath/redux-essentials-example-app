5c20523fa03053890ef6a753c91bd4072607b1d5
sangkamath <twinkle_kamath@yahoo.com>
2024-02-07 11:50:30 +0530

    Convert users query to store normalized response data

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,11 +1,11 @@
 import { createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import { apiSlice } from "../api/apiSlice";
 
-/*
+
 const usersAdapter = createEntityAdapter();
 
 const initialState = usersAdapter.getInitialState();
-
+/*
 export const fetchUsers = createAsyncThunk("users/fetchUsers", async() => {
     const response = await client.get("/fakeApi/users");
     return response.data;
@@ -34,7 +34,10 @@ export const extendedApiSlice =
 apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getUsers: builder.query({
-            query: () => "/users"
+            query: () => "/users",
+            transformResponse: (res) => {
+                return usersAdapter.setAll(initialState,res);
+            }
         })
     })
 })
@@ -43,15 +46,10 @@ export const { useGetUsersQuery} = extendedApiSlice;
 
 export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select();
 
-const emptyUsers = [];
-
-export const selectAllUsers = createSelector(
+const selectUsersData = createSelector(
     selectUsersResult,
-    (usersResult) => usersResult?.data ?? emptyUsers
+    usersResult => usersResult.data
 )
 
-export const selectUserById = createSelector(
-    selectAllUsers,
-    (state, userId) => userId,
-    (users, userId) => users.find((user) => user.id === userId)
-)
+export const { selectAll: selectAllUsers, selectById: selectUserById}
+= usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState);
