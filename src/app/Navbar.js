cba3b0c95900056df92f5843ff2fe436f6d8a98d
sangkamath <twinkle_kamath@yahoo.com>
2024-02-17 11:55:03 +0530

    Show notifications from cache and track client side read status

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchNotifications, selectAllNotifications } from '../features/notifications/notificationsSlice';
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import {
+  fetchNotificationsWebsocket,
+  selectNotificationsMetadata,
+  useGetNotificationsQuery,
+} from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
-  const dispatch = useDispatch();
-  const notifications = useSelector(selectAllNotifications);
-  const numUnreadNotifications = notifications.filter((n) => !n.read).length;
+  const dispatch = useDispatch()
+
+  // Trigger initial fetch of notifications and keep the websocket open to receive updates
+  useGetNotificationsQuery()
+
+  const notificationsMetadata = useSelector(selectNotificationsMetadata)
+  const numUnreadNotifications = notificationsMetadata.filter((n) => !n.read)
+    .length
 
   const fetchNewNotifications = () => {
-    dispatch(fetchNotifications())
+    dispatch(fetchNotificationsWebsocket())
   }
 
-  let unreadNotificationsBadge;
+  let unreadNotificationsBadge
 
-  if(numUnreadNotifications > 0) {
+  if (numUnreadNotifications > 0) {
     unreadNotificationsBadge = (
-      <span className='badge'>{numUnreadNotifications}</span>
+      <span className="badge">{numUnreadNotifications}</span>
     )
   }
 
@@ -29,9 +39,12 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
-            <Link to="/notifications">Notifications {unreadNotificationsBadge}</Link>
+            <Link to="/notifications">
+              Notifications {unreadNotificationsBadge}
+            </Link>
           </div>
-          <button className='button' onClick={fetchNewNotifications}>
+
+          <button className="button" onClick={fetchNewNotifications}>
             Refresh Notifications
           </button>
         </div>
--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,45 +1,55 @@
-import React,{ useLayoutEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { formatDistanceToNow, parseISO } from "date-fns";
+import React, { useLayoutEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { formatDistanceToNow, parseISO } from 'date-fns'
+import classnames from 'classnames'
 
-import { selectAllUsers } from "../users/usersSlice";
-import { selectAllNotifications, allNotificationsRead } from "./notificationsSlice";
-import classnames from "classnames";
+import { selectAllUsers } from '../users/usersSlice'
+
+import {
+  useGetNotificationsQuery,
+  allNotificationsRead,
+  selectMetadataEntities,
+} from './notificationsSlice'
 
 export const NotificationsList = () => {
-    const dispatch = useDispatch();
-    const notifications = useSelector(selectAllNotifications);
-    const users = useSelector(selectAllUsers);
+  const dispatch = useDispatch()
+  const { data: notifications = [] } = useGetNotificationsQuery()
+  const notificationsMetadata = useSelector(selectMetadataEntities)
+  const users = useSelector(selectAllUsers)
 
-    useLayoutEffect(() => {
-        dispatch(allNotificationsRead());
-    })
+  useLayoutEffect(() => {
+    dispatch(allNotificationsRead())
+  })
 
-    const renderedNotifications = notifications.map((notification) => {
-        const date = parseISO(notification.date);
-        const timeAgo = formatDistanceToNow(date);
-        const user = users.find((user) => user.id === notification.user) || { name: "Unknown user"}
+  const renderedNotifications = notifications.map((notification) => {
+    const date = parseISO(notification.date)
+    const timeAgo = formatDistanceToNow(date)
+    const user = users.find((user) => user.id === notification.user) || {
+      name: 'Unknown User',
+    }
 
-        const notificationClassname = classnames( "notification", {
-            new: notification.isNew
-        })
+    const metadata = notificationsMetadata[notification.id]
 
-        return (
-            <div key={notification.id} className={notificationClassname}>
-                <div>
-                    <b>{user.name}</b> {notification.message}
-                </div>
-                <div title={notification.date}>
-                    <i>{timeAgo} ago</i>
-                </div>
-            </div>
-        )
+    const notificationClassname = classnames('notification', {
+      new: metadata.isNew,
     })
 
     return (
-        <section className="notificationsList">
-            <h2>Notifications</h2>
-            {renderedNotifications}
-        </section>
+      <div key={notification.id} className={notificationClassname}>
+        <div>
+          <b>{user.name}</b> {notification.message}
+        </div>
+        <div title={notification.date}>
+          <i>{timeAgo} ago</i>
+        </div>
+      </div>
     )
+  })
+
+  return (
+    <section className="notificationsList">
+      <h2>Notifications</h2>
+      {renderedNotifications}
+    </section>
+  )
 }
